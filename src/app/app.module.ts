f13a5f2f2569608c083e59d8a46dc0ac29e99fb2
John Chappelear <john.chappelear@gmail.com>
2019-10-28 21:16:51 -0400

    Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent,
+    BlueComponent,
+    YellowComponent
   ],
   imports: [
     BrowserModule
